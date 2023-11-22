3fc58351407be85e09d43bc3c979df85043fd205
Lakssterius <lakssterius@gmail.com>
2023-11-22 16:02:38 +0300

    Фикс выдачи товара курьеру
    если игрок уже стоит у выдачи, подошедший курьер проверяет возможность взять товар

--- Assets/Scripts/Orders/DeliveryCounter.cs
+++ Assets/Scripts/Orders/DeliveryCounter.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class DeliveryCounter : MonoBehaviour
 {
     [SerializeField] LayerMask CourierMask;
-    [SerializeField] LayerMask CladoOneMask;
+    [SerializeField] LayerMask StorekeeperMask;
+
+    private Storage _storekeeperStorage;
 
     private void OnTriggerEnter(Collider other)
     {
         if (((1 << other.gameObject.layer) & CourierMask) > 0 && other.attachedRigidbody.TryGetComponent<Courier>(out var courier)) {
             CourierManager.SetAwaitingCourier(courier);
             OrdersManager.GenerateOrderForCourier(courier);
+
+            if (_storekeeperStorage)
+                CourierManager.SendGoodsToAwaiting(_storekeeperStorage);
         }
-        else if (((1 << other.gameObject.layer) & CladoOneMask) > 0 && other.attachedRigidbody.TryGetComponent<Storage>(out var storage)) {
+        else if (((1 << other.gameObject.layer) & StorekeeperMask) > 0 && other.attachedRigidbody.TryGetComponent<Storage>(out var storage)) {
+            _storekeeperStorage = storage;
             CourierManager.SendGoodsToAwaiting(storage);
         }
     }
