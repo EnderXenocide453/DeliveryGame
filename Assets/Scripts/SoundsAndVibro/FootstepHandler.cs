4125c06e5282c0732482b2392f5d449f2dd48d40
Lakssterius <lakssterius@gmail.com>
2023-12-04 15:31:24 +0300

    Уменьшил громкость. Шаги привязаны к настройкам звука

--- Assets/Scripts/SoundsAndVibro/FootstepHandler.cs
+++ Assets/Scripts/SoundsAndVibro/FootstepHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SettingAudio))]
 [RequireComponent(typeof(AudioSource))]
 public class FootstepHandler : MonoBehaviour
 {
